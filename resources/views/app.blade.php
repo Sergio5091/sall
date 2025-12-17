76f4f5f05d3447d425b54985e28a84b4b1ff2f87
Francis Frymce HOUNKPEVI <francisfrymcehounpkevi@gmail.com>
2025-12-17 21:55:12 +0100

    new changement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-white dark:bg-slate-900 text-slate-900 dark:text-white transition-colors duration-200">
         @inertia
     </body>
 </html>
